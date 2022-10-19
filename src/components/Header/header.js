cf0189b652adc2377c2c14610d826b21b4efba96
Marcelo <glacial@gmail.com>
2022-10-18 22:35:43 -0400

    fix typo

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,7 +2,7 @@ import styles from './headerStyles';
 
 const header = `
     <div class='header'>
-        <h1 class='${styles.title}'>Vite + Tailwind + Vanilla Js Boiletrplate</h1>
+        <h1 class='${styles.title}'>Vite + Tailwind + Vanilla Js Boilerplate</h1>
     </div>
 `;
 export default header;
